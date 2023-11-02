32c0969df11421c2e9ede44be2d7b2ae20a94fa4
yalomako <yalomako00@mail.ru>
2023-11-02 18:16:00 +0300

    sound to another folder

--- src/tasks/base_task.py
+++ src/tasks/base_task.py
@@ -7,7 +7,7 @@ class BaseTask(pg.sprite.Sprite):
         super().__init__(*groups)
         self.complete = False
         self.started = False
-        self.complete_sound = pg.mixer.Sound('sounds/task_completed.mp3')
+        self.complete_sound = pg.mixer.Sound('../sounds/task_completed.mp3')
         self.def_font = pg.font.SysFont("arial", 30, True)
         self.moving_sprites = pg.sprite.Group()
         self.press_f_image = self.def_font.render('Нажмите F', True, 'black')
